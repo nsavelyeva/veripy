9d8f4c37ef79bb33a710c63eccadab0c1173a450
Natallia Savelyeva <n.savelyeva@thebeat.co>
2022-01-26 08:37:44 +0100

    Use --cover-package option

--- src/scanners.py
+++ src/scanners.py
@@ -48,5 +48,5 @@ class Cov(Scanner):
     def __init__(self, path='.', options='--verbose --with-id', covgate=0):
         self.name = 'cov'
         self.description = 'runs unit tests with code coverage using [nose](https://nose.readthedocs.io/en/latest/) and optionally asserts quality gate'
-        self.command = f'nosetests {options} --with-coverage --where {path}'  # nosetests --verbose --with-id --with-coverage --where lib/tests
+        self.command = f'nosetests {options} --with-coverage --cover-package={path} --where {path}'  # nosetests --verbose --with-id --with-coverage --where lib/tests
         self.install('requirements.txt')
